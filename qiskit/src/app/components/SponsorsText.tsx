20fb1a245677d0e729257236d9c16507062e9bae
Harshita K <harshita-cmyk@users.noreply.github.com>
2025-09-09 10:49:24 +0530

    Made Sponsors text and Sponsors separate, fixed the color and links for footer

---
+++ qiskit/src/app/components/SponsorsText.tsx
@@ -0,0 +1,20 @@
+
+export default function BetweenPage() {
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center p-2 sm:p-4 lg:py-12">
+    <h2 
+  className="font-kantumruy text-4xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-6 md:mb-8 text-center"
+  style={{
+    background: 'linear-gradient(to right, #3b82f6, #8b5cf6, #f472b6, #8b5cf6, #3b82f6)',
+    WebkitBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    backgroundClip: 'text'
+  }}
+>
+  Our Sponsors
+</h2>
+  </div>
+</>
+  );
+}
--- qiskit/src/app/page.tsx
+++ qiskit/src/app/page.tsx
@@ -1,4 +1,5 @@
-import Sponsors from "./Components/SponsorsOnPage";
+import SponsorsText from "./Components/SponsorsText";
+import Sponsors from "./Components/Sponsors";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <Header />
       <main id="main-content">
+        <SponsorsText />
         <Sponsors />
         {/* Add other components or content here as needed */}
       </main>
