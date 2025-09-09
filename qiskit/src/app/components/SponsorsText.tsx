57b8aba43e622a79a70012f4917e52d48cd7088a
Harshita K <harshita-cmyk@users.noreply.github.com>
2025-09-09 11:44:52 +0530

    Sponsor box gradient

--- qiskit/src/app/components/SponsorsText.tsx
+++ qiskit/src/app/components/SponsorsText.tsx
@@ -3,7 +3,7 @@ export default function BetweenPage() {
     <>
     <div className="bg-black flex flex-col items-center justify-center p-2 sm:p-4 lg:pb-12 lg:pt-2">
     <h2 
-  className="font-kantumruy text-4xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-6 md:mb-8 text-center"
+  className="font-kantumruy text-4xl sm:text-3xl md:text-4xl font-semibold mb-4 sm:mb-6 md:mb-8 text-center"
   style={{
     background: 'linear-gradient(to right, #3b82f6, #8b5cf6, #f472b6, #8b5cf6, #3b82f6)',
     WebkitBackgroundClip: 'text',
