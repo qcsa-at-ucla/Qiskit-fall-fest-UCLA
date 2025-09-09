d6a9c37cf0dfb4ee50418c78bb61d8843b4fc6ef
emz95 <emma91zhang@gmail.com>
2025-09-09 14:54:29 -0700

    gradient placeholder

--- qiskit/src/app/components/SpeakerCard.tsx
+++ qiskit/src/app/components/SpeakerCard.tsx
@@ -6,13 +6,20 @@ type SpeakerProps = {
 
 export default function SpeakerCard({name, img} : SpeakerProps ) {
   return (
-    <div className="flex flex-col items center w-40">
-        <div className="w-40 h-40 rounded-full bg-white overflow-hidden flex items-center justify-center">
+    <div className="flex flex-col items-center w-40">
+
+        <div className="w-40 h-40 rounded-full bg-white overflow-hidden flex items-center justify-center"
+                style={{
+                  background:
+                    "linear-gradient(to right, #C3B5FF, #DFB1F9, #F0B3EB, #DFB1F9, #C3B5FF)",
+                }}>
+          {/*
             <img
             src={img}
             alt={name}
             className="w-full h-full object-cover"
             />
+          */}
         </div>
         <p className="mt-8 text-white text-center w-full">{name}</p>
     </div>
