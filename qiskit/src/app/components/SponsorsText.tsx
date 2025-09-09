e0a73101c903f0330d7fcceb67179287ae60da0a
Harshita K <harshita-cmyk@users.noreply.github.com>
2025-09-09 10:56:46 +0530

    Changed background to black

--- qiskit/src/app/components/SponsorsText.tsx
+++ qiskit/src/app/components/SponsorsText.tsx
@@ -1,8 +1,7 @@
-
 export default function BetweenPage() {
   return (
     <>
-    <div className="flex flex-col items-center justify-center p-2 sm:p-4 lg:py-12">
+    <div className="bg-black flex flex-col items-center justify-center p-2 sm:p-4 lg:pb-12 lg:pt-2">
     <h2 
   className="font-kantumruy text-4xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-6 md:mb-8 text-center"
   style={{
