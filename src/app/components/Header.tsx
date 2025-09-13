4982e97b188d9356d1e0191287ee69ae176a6018
Harshita K <harshita-cmyk@users.noreply.github.com>
2025-09-13 13:52:52 +0530

    Removed the event tab

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -53,12 +53,6 @@ export default function MainWebsiteHeader() {
               href="/schedule" 
               className="text-white hover:text-gray-200 font-semibold text-lg transition-colors duration-200"
             >
-              Event
-            </Link>
-            <Link 
-              href="/team" 
-              className="text-white hover:text-gray-200 font-semibold text-lg transition-colors duration-200"
-            >
               Team
             </Link>
             <Link 
@@ -114,13 +108,6 @@ export default function MainWebsiteHeader() {
               </Link>
               <Link 
                 href="/events" 
-                className="text-white hover:text-black font-semibold text-lg transition-colors duration-200 py-2 px-2 hover:bg-white hover:bg-opacity-20 rounded-md"
-                onClick={() => setIsMobileMenuOpen(false)}
-              >
-                Event
-              </Link>
-              <Link 
-                href="/team" 
                 className="text-white hover:text-black font-semibold text-lg transition-colors duration-200 py-2 px-2 hover:bg-white hover:bg-opacity-20 rounded-md"
                 onClick={() => setIsMobileMenuOpen(false)}
               >
